204248cc9a010e3e81243d2a0930433aec496a44
edoardope <pencoedoardo@gmail.com>
2023-04-04 00:28:42 +0200

    v2 giocabile con contatore mine adiacenti

--- main.js
+++ main.js
@@ -32,6 +32,75 @@ function aggiornaPunteggio() {
   return punteggio
 }
 
+//funzione controllo e registro numero bombe caselle adiacenti
+
+function contrcaselleadiacenti(y, difset){
+
+  let casselleadiacenti = []
+
+  if ((y == 11 || y == 21 || y == 31 || y == 41 || y == 51 || y == 61 || y == 71 || y == 81) && (difset == diff1)) {
+    casselleadiacenti.push(y + 1) 
+    casselleadiacenti.push(y + 10)
+    casselleadiacenti.push(y + 11)
+    casselleadiacenti.push(y - 9)
+    casselleadiacenti.push(y - 10)
+  } else if((y == 20 || y == 30 || y == 40 || y == 50 || y == 60 || y == 70 || y == 80 || y == 90) && (difset == diff1)){
+    casselleadiacenti.push(y + 9)
+    casselleadiacenti.push(y + 10)
+    casselleadiacenti.push(y - 1)
+    casselleadiacenti.push(y - 10)
+    casselleadiacenti.push(y - 11)
+  } else if((y == 2 || y == 3 || y == 4 || y == 5 || y == 6 || y == 7 || y == 8 || y == 9) && (difset == diff1)){
+    casselleadiacenti.push(y + 1)
+    casselleadiacenti.push(y + 9)
+    casselleadiacenti.push(y + 10)
+    casselleadiacenti.push(y + 11)
+    casselleadiacenti.push(y - 1)
+  } else if ((y == 92 || y == 93 || y == 94 || y == 95 || y == 96 || y == 97 || y == 98 || y == 99) && (difset == diff1)){
+    casselleadiacenti.push(y + 1)
+    casselleadiacenti.push(y - 9)
+    casselleadiacenti.push(y - 10)
+    casselleadiacenti.push(y - 11)
+    casselleadiacenti.push(y - 1)
+  } else if((y == 1) && (difset == diff1)){
+    casselleadiacenti.push(y + 1) 
+    casselleadiacenti.push(y + 10)
+    casselleadiacenti.push(y + 11)
+  } else if((y == 91) && (difset == diff1)){
+    casselleadiacenti.push(y + 1) 
+    casselleadiacenti.push(y - 9)
+    casselleadiacenti.push(y - 10)
+  } else if((y == 10) && (difset == diff1)){
+    casselleadiacenti.push(y + 9) 
+    casselleadiacenti.push(y + 10)
+    casselleadiacenti.push(y - 1)
+  } else if((y == 100) && (difset == diff1)){
+    casselleadiacenti.push(y - 10) 
+    casselleadiacenti.push(y - 9)
+    casselleadiacenti.push(y - 1)
+  } else {
+    casselleadiacenti.push(y + 1)
+    casselleadiacenti.push(y + 9)
+    casselleadiacenti.push(y + 10)
+    casselleadiacenti.push(y + 11)
+    casselleadiacenti.push(y - 1)
+    casselleadiacenti.push(y - 9)
+    casselleadiacenti.push(y - 10)
+    casselleadiacenti.push(y - 11)
+  }
+
+  let contatore = 0
+
+  for (let i = 0; i < difset.length; i++) {
+    if (casselleadiacenti.includes(difset[i])) {
+      contatore++;
+    }
+  }
+
+  return contatore
+  
+}
+
 //funzione per generare i singoli elementi div con classe e difficoltà per gestire il comportameto all click 
 
 function generabox(y, classe, difficoltà) {
@@ -39,6 +108,9 @@ function generabox(y, classe, difficoltà) {
   box.classList.add(classe);
   box.innerHTML = y;
   box.addEventListener("click", function() {
+
+    // contrcaselleadiacenti(y, diff1)
+    let = nuovonumero = contrcaselleadiacenti(y, diff1)
         
     if (difficoltà.includes(y)) { // controlla se il numero è presente nell'array numeriCasuali
       
@@ -56,8 +128,11 @@ function generabox(y, classe, difficoltà) {
       box.classList.add("blue");
       casellevuoteselezionate.push(y)
       aggiornaPunteggio()
+      box.innerHTML = nuovonumero;
 
     }
+
+    
   });
   return box;
 }
